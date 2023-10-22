476b4fd284125ca8f556cfbea7d01491eee30722
Kawthar Badran <37708354+kawtharbadran@users.noreply.github.com>
2023-10-22 00:10:45 -0400

    Uploading utilities given in google example

---
+++ utils.py
@@ -0,0 +1,147 @@
+# Utils
+import time
+from typing import List
+from typing import Optional
+
+# Langchain
+import langchain
+from pydantic import BaseModel
+
+print(f"LangChain version: {langchain.__version__}")
+
+# Vertex AI
+from google.cloud import aiplatform
+from langchain.chat_models import ChatVertexAI
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.llms import VertexAI
+from langchain.schema import HumanMessage, SystemMessage
+
+print(f"Vertex AI SDK version: {aiplatform.__version__}")
+
+
+
+PROJECT_ID = "badran-3660-f23"
+LOCATION = "us-central1"
+
+import vertexai
+x = vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+# def init_sample(
+#     project: Optional[str] = None,
+#     location: Optional[str] = None,
+#     experiment: Optional[str] = None,
+#     staging_bucket: Optional[str] = None,
+#     credentials: Optional[auth_credentials.Credentials] = None,
+#     encryption_spec_key_name: Optional[str] = None,
+#     service_account: Optional[str] = None,
+# ):
+    
+#     from google.cloud import aiplatform
+
+#     aiplatform.init(
+#         project=project,
+#         location=location,
+#         experiment=experiment,
+#         staging_bucket=staging_bucket,
+#         credentials=credentials,
+#         encryption_spec_key_name=encryption_spec_key_name,
+#         service_account=service_account,
+#     )
+
+# Utility functions for Embeddings API with rate limiting
+def rate_limit(max_per_minute):
+    period = 60 / max_per_minute
+    print("Waiting")
+    while True:
+        before = time.time()
+        yield
+        after = time.time()
+        elapsed = after - before
+        sleep_time = max(0, period - elapsed)
+        if sleep_time > 0:
+            print(".", end="")
+            time.sleep(sleep_time)
+
+
+class CustomVertexAIEmbeddings(VertexAIEmbeddings, BaseModel):
+    requests_per_minute: int
+    num_instances_per_batch: int
+
+    # Overriding embed_documents method
+    def embed_documents(self, texts: List[str]):
+        limiter = rate_limit(self.requests_per_minute)
+        results = []
+        docs = list(texts)
+
+        while docs:
+            # Working in batches because the API accepts maximum 5
+            # documents per request to get embeddings
+            head, docs = (
+                docs[: self.num_instances_per_batch],
+                docs[self.num_instances_per_batch :],
+            )
+            chunk = self.client.get_embeddings(head)
+            results.extend(chunk)
+            next(limiter)
+
+        return [r.values for r in results]
+    
+    # Utility functions for Embeddings API with rate limiting
+def rate_limit(max_per_minute):
+    period = 60 / max_per_minute
+    print("Waiting")
+    while True:
+        before = time.time()
+        yield
+        after = time.time()
+        elapsed = after - before
+        sleep_time = max(0, period - elapsed)
+        if sleep_time > 0:
+            print(".", end="")
+            time.sleep(sleep_time)
+
+
+class CustomVertexAIEmbeddings(VertexAIEmbeddings, BaseModel):
+    requests_per_minute: int
+    num_instances_per_batch: int
+
+    # Overriding embed_documents method
+    def embed_documents(self, texts: List[str]):
+        limiter = rate_limit(self.requests_per_minute)
+        results = []
+        docs = list(texts)
+
+        while docs:
+            # Working in batches because the API accepts maximum 5
+            # documents per request to get embeddings
+            head, docs = (
+                docs[: self.num_instances_per_batch],
+                docs[self.num_instances_per_batch :],
+            )
+            chunk = self.client.get_embeddings(head)
+            results.extend(chunk)
+            next(limiter)
+
+        return [r.values for r in results]
+    
+
+# LLM model
+llm = VertexAI(
+    model_name="text-bison@001",
+    max_output_tokens=512,
+    temperature=0.1,
+    top_p=0.8,
+    top_k=40,
+    verbose=True,
+)
+
+# Chat
+chat = ChatVertexAI()
+
+# Embedding
+EMBEDDING_QPM = 15
+EMBEDDING_NUM_BATCH = 2
+embeddings = CustomVertexAIEmbeddings(
+    requests_per_minute=EMBEDDING_QPM,
+    num_instances_per_batch=EMBEDDING_NUM_BATCH,
+)
